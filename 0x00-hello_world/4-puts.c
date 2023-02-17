3e14a419b9f3b5278837bea516239a88b0800028
sempo <sempo.code@gmail.com>
2023-02-16 17:45:41 -0800

    Using "puts" method.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this the main function
+ * Return: always gives a zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
