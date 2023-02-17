8b95ed10069bd277e5e7807108d3e5e95db2c428
sempo <sempo.code@gmail.com>
2023-02-16 19:02:35 -0800

    printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0 when auccessful
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+
+}
