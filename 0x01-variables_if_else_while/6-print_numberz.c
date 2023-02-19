3cfe18a9dca7944c7b5fd63e7d9eb5310221c0f0
sempo <sempo.code@gmail.com>
2023-02-19 14:23:50 -0800

    Numbers as characters

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is thee function main
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	int t;
+
+	for (t = '0'; t <= '9'; t++)
+	{
+		putchar(t);
+	}
+	putchar('\n');
+	return (0);
+}
