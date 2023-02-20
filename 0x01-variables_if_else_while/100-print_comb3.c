e64f1d552b2184fa805b11242d6ac774c5ccc292
sempo <sempo.code@gmail.com>
2023-02-20 08:00:45 -0800

    Printing combinations of two digits without repating any using putchar

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,6 @@ int main(void)
 	int i;
 	int j;
 
-
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 1; j < 10; j++)
@@ -17,7 +16,7 @@ int main(void)
 			if (i < j && i != j)
 			{
 				putchar(i + '0');
-				putchar(j + '0');	
+				putchar(j + '0');
 				if (i + j != 17)
 				{
 				putchar(',');
