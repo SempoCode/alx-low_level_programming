f10923fce22dbe9e55f02803395f69241d92745e
sempo <sempo.code@gmail.com>
2023-02-22 18:47:19 -0800

    This is the header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - this is the main function
+ * _putchar: prints the alphabet putchar
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
