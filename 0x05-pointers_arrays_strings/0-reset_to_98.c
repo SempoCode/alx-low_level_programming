3822619924608f73e4da33ea5a6c3781ea88bce3
sempo <sempo.code@gmail.com>
2023-02-27 15:48:13 -0800

    Updating the valur of a variable using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that updates the value it points to to 98
+ * @n: This a pointer
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
