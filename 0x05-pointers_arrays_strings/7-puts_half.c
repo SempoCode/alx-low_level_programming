731d785423160cc5e20a4373b94d2fc1a602cc1d
sempo <sempo.code@gmail.com>
2023-02-28 13:15:13 -0800

    Printing a half of the string correctly

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,26 +7,29 @@
 void puts_half(char *str)
 {
 	int i;
-	int length = 0;
+	int length;
 	int n;
 
-	for (i = 0; *(str + i) != '\0'; ++i)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		length += i;
+		length = i;
 	}
 
 	if (length % 2 == 0)
 	{
 		n = length / 2;
+		for (i = n ; i <= length; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 	else
 	{
 		n = (length - 1) / 2;
-
-	for (i = n; i < length; i++)
-	{
-		_putchar(str[i]);
+		for (i = n + 1 ; i <= length; i++)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
-	}
 }
