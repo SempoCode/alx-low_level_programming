9d9fb4350ec366cc1ec8f874220841f4566e4ec6
sempo <sempo.code@gmail.com>
2023-02-27 18:06:57 -0800

    Counting the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - function that measures length of strimgs
+ * @s: the pointer to the string
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != '\0'; ++i)
+	{
+
+	}
+	return (i);
+}
