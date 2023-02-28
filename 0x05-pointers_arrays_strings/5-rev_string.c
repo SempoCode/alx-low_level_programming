75d172710bec1543508e9a09c71502ff6c3238a6
sempo <sempo.code@gmail.com>
2023-02-28 15:26:00 -0800

    Reversing the string complete ...

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * rev_string - This prints the second half of the string
- * @str: This is the pointer to the string
+ * @s: This is the pointer to the string
  * Return: nothing
  */
 void rev_string(char *s)
@@ -11,20 +11,19 @@ void rev_string(char *s)
 	char *start, *end, temp;
 
 	for (i = 0; s[i] != '\0'; i++)
-        {
-                length = i;
-        }
+	{
+		length = i;
+	}
 
 	start = s;
 	end = s;
 
-	for (i = 0; i <length; i++)
+	for (i = 0; i < length; i++)
 	{
 		end++;
 	}
-	
-	for (i = 0; i < length / 2; i ++)
-	{	
+	for (i = 0; i < length / 2; i++)
+	{
 		temp = *end;
 		*end = *start;
 		*start = temp;
