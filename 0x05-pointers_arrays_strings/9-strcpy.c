964578f1bda740c104504e0b6256f6e5af889815
sempo <sempo.code@gmail.com>
2023-02-28 17:22:06 -0800

    Copying one pointer to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcpy - thsi function copies a string
+ * @dest: pointer to where the string is copied to
+ * @src: pointer to where the string is copied from
+ * Return: Characters when successful
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, leng;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		leng = i;
+	}
+	for (i = 0; i <= leng; i++)
+	{
+		dest[i] = src[i];
+	}
+	return dest;
+}
