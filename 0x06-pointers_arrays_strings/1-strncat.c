f37b916bca1976a5ed61628519260d436bc2d35a
sempo <sempo.code@gmail.com>
2023-03-02 15:04:38 -0800

    cooncatinating strings of give bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - function which concatenates strings
+ * @dest: parameter
+ * @src: parameter
+ * @n: parameter
+ * Return: returns char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
