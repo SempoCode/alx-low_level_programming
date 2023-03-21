77abbdef0bc60f9fa1f7ac1cfea7409d5a48116f
sempo <sempo.code@gmail.com>
2023-03-21 05:18:06 -0700

    The structure sata type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,15 @@
-#ifndef DOH_H
+#ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - This is a structure
+ * @name: parameter1
+ * @age: parameter2
+ * @owner: parameter2
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-#endif
+};
+#endif /* DOG */
