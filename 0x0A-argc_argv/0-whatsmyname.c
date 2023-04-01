171aad7bcc0cc3f97be0a0b5f0ddbaa2f056ec5e
Sempo <sempo.code@gmail.com>
2023-04-01 13:56:53 -0700

    Printing my own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function printing own name
+ * @argv: array of the arguments
+ * @argc: number of the arguments
+ * Return: 0 when successful
+ */
+int main(int __attribute__((unused)) argc, char* argv[])
+{
+	printf("%s.\n", *argv);
+	retutn (0);
+}
