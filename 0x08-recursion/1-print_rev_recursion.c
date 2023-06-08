56b5401525fa0d8f7b515b9839e402b031c18e34
Sempo <sempo.code@gmail.com>
2023-06-08 16:08:56 -0700

    printing in reverse recursively

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function to reverse the string
+ * @s: pointer to the  string to reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
