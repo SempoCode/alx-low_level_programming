0f19b5401d51bd378b6922e8afc042d3ef08800e
Sempo <sempo.code@gmail.com>
2023-06-11 01:55:13 -0700

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function printing own name
+ * @argc: number of the arguments
+ * @argv: array of pointers to the arguments
+ * Return: 0 when successful
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
