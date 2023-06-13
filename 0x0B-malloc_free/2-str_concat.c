177dc0a9306d38cbd9ff395a52772397a854aca3
Sempo <sempo.code@gmail.com>
2023-06-13 06:25:32 -0700

    two string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,22 +11,23 @@ char *str_concat(char *s1, char *s2)
 	int i, n1, n2;
 	char *s;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		n1 = i;
 	for (i = 0; s2[i] != '\0'; i++)
 		n2 = i;
-	if (s1 == NULL)
-		n1 = 0;
-	if (s2 == NULL)
-		n2 = 0;
 	s = malloc(sizeof(char) * (n1 + n2 + 3));
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
-	for (i = 0; s2[i]; i++)
-		s[i + n1] = s2[i];
+	n1 += 1;
+	for (i = 0; s2[i] != '\0'; i++)
+		s[n1 + i] = s2[i];
 	return (s);
 	free(s);
 }
